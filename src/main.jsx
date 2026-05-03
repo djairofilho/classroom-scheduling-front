e866f13dc50fd2b15e352ddad194c1d95a21d05b
djairofilho <djairodsfilho@gmail.com>
2026-05-03 00:56:09 -0300

    feat: mount auth provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { I18nProvider } from './i18n/I18nProvider.jsx'
+import { AuthProvider } from './lib/auth.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <I18nProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
     </I18nProvider>
   </StrictMode>,
 )
